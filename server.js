d4fe5f843d5c0286ce10f7d5e94b32ee5e083fe7
djemerson <djemerson@gmail.com>
2018-04-28 20:19:45 -0500

    push to heroku

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 var express = require("express");
 var body = require("body-parser");
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(body.urlencoded({ extended: false }));
+
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
+
+app.listen(port);
